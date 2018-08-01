2fa1d3122ebaf87c7b07ed723960f33868f691e2
YK <ykagal@gmail.com>
2018-08-01 13:35:40 -0400

    Add GET/todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -34,12 +35,40 @@ app.post('/todos', (req, res) =>{
 //     });
 // });
 
+//GET all Todos
+
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/123431
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    //res.send(req.params);
+
+    //validate ID using isValid
+    //Stop function, respond with 404 - send back empty value
+    if (!ObjectID.isValid) {
+        res.status(404).send();
+    }
+
+    //findById
+        //success
+            //if todo - send it back
+            // if no todo - send back 404 with empty body
+        //error
+            //400 - send empty body back
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        //instead of just sending todo, send it as an object that has a todo property, which allows you to do other stuff with it
+        res.send({todo});
+    }).catch((e) => res.status(404).send());
 });
 
 app.listen(3000, () => {
