e85915669ff156817fd7405d9e79a774ba39a995
YK <ykagal@gmail.com>
2018-08-01 21:16:30 -0400

    Fix port for heroku

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(404).send());
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Started up on port ${port}`);
 });
 
